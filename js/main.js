d25a2dfe8322b20004d29523669bc778b3f8fb82
Shuvo <shahshuvo@yahoo.com>
2017-06-21 01:43:49 +0600

    Major modifications

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ $(document).ready(() => {
 function getMovies(searchText){
   axios.get('https://api.themoviedb.org/3/search/movie?api_key=771c671cd8e98c6b1fa8bc36fc4ce71a&query='+searchText)
     .then((response) => {
-      console.log(response);
       let movies = response.data.results;
       let output = '';
       $.each(movies, (index, movie) => {
@@ -17,7 +16,7 @@ function getMovies(searchText){
           <div class="col-md-3">
             <div class="well text-center">
               <img src="https://image.tmdb.org/t/p/w640/${movie.poster_path}">
-              <h3>${movie.title}</h3>
+              <p><strong>${movie.title}</strong></p>
               <a onclick="movieSelected('${movie.id}')" class="btn btn-primary btn-xs">Movie Details</a>
             </div>
           </div>
@@ -40,17 +39,27 @@ function movieSelected(id){
 function getMovie(){
   let movieId = sessionStorage.getItem('movieId');
 
-  axios.get('https://api.themoviedb.org/3/search/movie/'+movieId+'?api_key=771c671cd8e98c6b1fa8bc36fc4ce71a')
+  axios.get('https://api.themoviedb.org/3/movie/'+movieId+'?api_key=771c671cd8e98c6b1fa8bc36fc4ce71a')
     .then((response) => {
-      console.log(response);
-      let movies = response.data.results;
+      let movie = response.data;
       let output = `
         <div class="row">
           <div class="col-md-4">
-            <img src="https://image.tmdb.org/t/p/w640/${movie.poster_path}" class="thumbnail">
+            <img src="https://image.tmdb.org/t/p/w640/${movie.poster_path}" class="thumbnail" style="margin-top:40px">
           </div>
           <div class="col-md-8">
-
+            <h2>${movie.title}</h2>
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Tagline: </strong>${movie.tagline}</li>
+              <li class="list-group-item"><strong>Release Date: </strong>${movie.release_date}</li>
+              <li class="list-group-item"><strong>Language: </strong>${movie.original_language}</li>
+              <li class="list-group-item"><strong>Rating: </strong>${movie.vote_average}</li>
+            </ul>
+            <div class="jumbotron">
+              <h3>Overview</h3>
+              <p>${movie.overview}</p>
+              <a href="http://www.imdb.com/title/${movie.imdb_id}" target=_blank class="btn btn-success">View on IMDb</a>
+            </div>
           </div>
         </div>
       `;
